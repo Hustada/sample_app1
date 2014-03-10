17fa33a940b933a5e306291650659cfbd684fd15
Hustada <hustada80@gmail.com>
2014-03-10 15:05:46 -0500

    all tests currently passing

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,34 +1,39 @@
-require 'spec_helper'
+  require 'spec_helper'
 
-describe "Authentication" do
+  describe "Authentication" do
 
-  subject { page }
+    subject { page }
 
-  describe "signin page" do
-    before { visit signin_path }
+    describe "signin page" do
+      before { visit signin_path }
 
-    it { should have_content('Sign in') }
-    it { should have_title('Sign in') }
-  end
-
-  describe 'signin' do
-    before { visit signin_path }
-
-    describe "with invalid information" do
-      before { click_button "Sign in" }
-
+      it { should have_content('Sign in') }
       it { should have_title('Sign in') }
-      it { should have_selector('div.alert.alert-error') }
+    end
 
-    describe "after visiting another page" do
-      before { click_link "Home" }
-      it { should_not have_selector('div.alert.alert-error') }
-      end
+    describe 'signin' do
+      before { visit signin_path }
 
+      describe "with invalid information" do
+        before { click_button "Sign in" }
 
-    describe "followed by signout" do
-      before { click_link 'Sign out' }
-      it {should have_link('Sign in') }
+        it { should have_title('Sign in') }
+        it { should have_selector('div.alert.alert-error') }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+        end
+
+      describe "with valid information" do
+
+        before do
+          visit signup_path
+          fill_in "Name",         with: "Example User"
+          fill_in "Email",        with: "user@example.com"
+          fill_in "Password",     with: "foobar"
+          fill_in "Password confirmation", with: "foobar"
+        end
       end
     end
   end

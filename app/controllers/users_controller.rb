d0c239f53727a15a6ab6a061c1ea2ae593fa0c08
Hustada <hustada80@gmail.com>
2014-03-11 16:38:40 -0500

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
   class UsersController < ApplicationController
+    before_action :signed_in_user, only: [:edit, :update]
+    before_action :correct_user,   only: [:edit, :update]
+
     def new
       @user = User.new
     end
@@ -27,6 +30,8 @@
     def update
       @user = User.find(params[:id])
       if @user.update_attributes(user_params)
+        flash[:success] = "Profile updated"
+        redirect_to @user
         #handle successful update
       else
       render 'edit'
@@ -38,5 +43,16 @@
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  #before filters
+
+  def signed_in_user
+    redirect_to signin_url, notice: "Please sign in." unless signed_in? 
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
